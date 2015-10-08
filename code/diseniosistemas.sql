bade5d720ba23631f3164f298413b52e1767e61c
LucasMangano <luks_manga@hotmail.com>
2015-10-07 22:07:21 -0300

    Foreign Keys
    Relaciones Creadas y base normalizada

--- code/diseniosistemas.sql
+++ code/diseniosistemas.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-08-2015 a las 22:22:04
+-- Tiempo de generación: 08-10-2015 a las 03:04:15
 -- Versión del servidor: 5.6.25
 -- Versión de PHP: 5.6.11
 
@@ -24,19 +24,6 @@ DELIMITER $$
 --
 -- Procedimientos
 --
-CREATE DEFINER=`root`@`localhost` PROCEDURE `VerPreferencias`(IN `IdUsu` INT)
-    NO SQL
-SELECT IdPreferencia, preferencias.IdIngrediente, IdUsuario, ingredientes.Ingrediente FROM preferencias inner join ingredientes on preferencias.IdIngrediente=ingredientes.IdIngrediente
-where idusuario=IdUsu$$
-
-DELIMITER ;
-
---
-
--- Propongo normalizar los nombres de los procedimientos almacenados de la forma sp_Nombre
-
-DELIMITER ;;
-
 CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_RegistrarUsuario`(
     IN usuario_param VARCHAR(50),
     IN pass_param VARCHAR(25),
@@ -66,8 +53,14 @@ insert into usuarios
 VALUES
 (usuario_param,pass_param,NOW(),idComplexion_var,sexo_param,idRutina_var,edad_param,altura_param,idPreexistentes_var,idDieta_var,`email_param`);
 
-END
-;;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `VerPreferencias`(IN `IdUsu` INT)
+    NO SQL
+SELECT IdPreferencia, preferencias.IdIngrediente, IdUsuario, ingredientes.Ingrediente FROM preferencias inner join ingredientes on preferencias.IdIngrediente=ingredientes.IdIngrediente
+where idusuario=IdUsu$$$$
+
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -229,6 +222,18 @@ CREATE TABLE IF NOT EXISTS `grupos` (
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `historial-horarios`
+--
+
+CREATE TABLE IF NOT EXISTS `historial-horarios` (
+  `IdRecetaHorario` int(11) NOT NULL,
+  `IdHistorial` int(11) NOT NULL,
+  `IdHorario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `historiales`
 --
 
@@ -238,8 +243,7 @@ CREATE TABLE IF NOT EXISTS `historiales` (
   `IdUsuario` int(11) NOT NULL,
   `IdReceta` int(11) NOT NULL,
   `Fecha de Accion` datetime NOT NULL,
-  `Fecha de Utilizacion` date NOT NULL,
-  `IdHorario` int(11) NOT NULL
+  `Fecha de Utilizacion` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -353,7 +357,14 @@ CREATE TABLE IF NOT EXISTS `pesos-ideales` (
   `Peso` int(11) NOT NULL,
   `PesoMinimo` int(11) NOT NULL,
   `PesoMaximo` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `pesos-ideales`
+--
+
+INSERT INTO `pesos-ideales` (`IdPesos-Ideales`, `Sexo`, `Altura`, `MedidaTorax`, `MedidaCintura`, `MedidaCadera`, `Peso`, `PesoMinimo`, `PesoMaximo`) VALUES
+(1, 'M', 183, 90, 75, 75, 80, 73, 88);
 
 -- --------------------------------------------------------
 
@@ -399,15 +410,16 @@ CREATE TABLE IF NOT EXISTS `preferencias` (
   `IdIngrediente` int(11) NOT NULL,
   `IdUsuario` int(11) NOT NULL,
   `Fecha` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `preferencias`
 --
 
 INSERT INTO `preferencias` (`IdPreferencia`, `IdIngrediente`, `IdUsuario`, `Fecha`) VALUES
-(1, 2, 29, '2015-08-24 00:00:00'),
-(2, 1, 29, '2015-08-04 00:00:00');
+(1, 4, 29, '2015-09-16 00:00:00'),
+(2, 7, 29, '2015-10-01 00:00:00'),
+(3, 14, 29, '2015-10-06 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -417,11 +429,9 @@ INSERT INTO `preferencias` (`IdPreferencia`, `IdIngrediente`, `IdUsuario`, `Fech
 
 CREATE TABLE IF NOT EXISTS `procedimiento` (
   `IdProcedimiento` int(11) NOT NULL,
-  `IdPaso1` int(11) NOT NULL,
-  `IdPaso2` int(11) NOT NULL,
-  `IdPaso3` int(11) NOT NULL,
-  `IdPaso4` int(11) NOT NULL,
-  `IdPaso5` int(11) NOT NULL
+  `IdPaso` int(11) NOT NULL,
+  `Paso` int(11) NOT NULL,
+  `IdReceta` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -453,18 +463,6 @@ CREATE TABLE IF NOT EXISTS `receta-estaciones` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `receta-horarios`
---
-
-CREATE TABLE IF NOT EXISTS `receta-horarios` (
-  `IdRecetaHorario` int(11) NOT NULL,
-  `IdReceta` int(11) NOT NULL,
-  `IdHorario` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `receta-ingredientes`
 --
 
@@ -486,7 +484,6 @@ CREATE TABLE IF NOT EXISTS `recetas` (
   `Receta` varchar(50) NOT NULL,
   `IdDificultad` int(11) NOT NULL,
   `IdUsuario` int(11) NOT NULL,
-  `IdProcedimiento` int(11) NOT NULL,
   `IdPiramide` int(11) NOT NULL,
   `IdDieta` int(11) NOT NULL,
   `Calorias` int(11) NOT NULL
@@ -546,46 +543,47 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `Altura` int(11) NOT NULL,
   `IdPreexistente` int(11) NOT NULL,
   `IdDieta` int(11) NOT NULL,
-  `Email` varchar(40) NOT NULL
+  `Email` varchar(40) NOT NULL,
+  `IdPesos-Ideales` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`IdUsuario`, `Usuario`, `Contrase`, `fechaCreacion`, `IdContextura`, `Sexo`, `Trabajo`, `IdRutina`, `Edad`, `Altura`, `IdPreexistente`, `IdDieta`, `Email`) VALUES
-(1, 'Leandrin', 'pepe', '0000-00-00 00:00:00', 3, 'm', NULL, 3, 0, 123, 1, 5, 'pepin'),
-(3, 'd', 'pepe', '2015-08-23 16:42:59', 3, 'm', NULL, 3, 0, 123, 1, 5, 'pepin'),
-(4, '425', 'twr', '2015-08-23 17:02:37', 1, 'm', NULL, 3, 0, 0, 3, 4, '24524'),
-(5, 'fd', 'fda', '2015-08-23 17:05:43', 2, 'f', NULL, 3, 0, 452, 3, 4, 'fafd'),
-(6, 'fdafad', 'fdafa', '2015-08-23 17:10:53', 2, 'm', NULL, 1, 0, 134, 3, 4, 'adfad'),
-(8, 'fdafadd', 'fdafa', '2015-08-23 17:11:37', 2, 'm', NULL, 1, 0, 134, 3, 4, 'adfad'),
-(9, 'fdafaddd', 'fdafa', '2015-08-23 17:12:15', 2, 'm', NULL, 1, 0, 134, 3, 4, 'adfad'),
-(10, 'fdafadddg', 'fdafa', '2015-08-23 17:12:28', 2, 'm', NULL, 1, 0, 134, 3, 4, 'adfad'),
-(11, 'frgsfg', 'gsfgf', '2015-08-23 17:20:23', 1, 'f', NULL, 1, 0, 34, 3, 4, 'sfgsf'),
-(12, 'frgsfgh', 'gsfgf', '2015-08-23 17:20:45', 1, 'f', NULL, 1, 0, 34, 3, 4, 'sfgsf'),
-(13, 'the', 'hethe', '2015-08-23 17:21:54', 2, 'm', NULL, 2, 0, 2142, 3, 4, 'hethe'),
-(15, '4254', '24524', '2015-08-23 17:26:56', 1, 'm', NULL, 1, 0, 2452, 3, 4, '452452'),
-(16, '789', '789', '2015-08-23 17:28:16', 1, 'm', NULL, 3, 0, 789, 3, 4, '7789'),
-(17, '46', '4', '2015-08-23 17:30:14', 2, 'm', NULL, 2, 0, 34, 3, 4, '4'),
-(18, '423', '23', '2015-08-23 17:32:18', 1, 'm', NULL, 2, 0, 234, 3, 4, '234'),
-(19, 'er', 're', '2015-08-23 17:34:25', 2, 'm', NULL, 2, 0, 4, 3, 4, 're'),
-(20, 'feeqeq', 'fe', '2015-08-23 17:36:23', 1, 'm', NULL, 2, 0, 3, 3, 4, 'qeqeq'),
-(21, '2', '23', '2015-08-23 17:39:52', 1, 'm', NULL, 2, 0, 3, 3, 4, '234'),
-(22, '25', '23', '2015-08-23 17:40:47', 1, 'm', NULL, 2, 0, 3, 3, 4, '234'),
-(23, 'g', 'g', '2015-08-23 17:42:38', 1, 'f', NULL, 1, 0, 34, 3, 4, 'r'),
-(25, 'gt', 'gttt', '2015-08-23 17:45:01', 1, 'f', NULL, 1, 0, 34, 3, 4, 'r'),
-(26, 'gt2', 'gttt', '2015-08-23 17:46:01', 1, 'f', NULL, 1, 0, 34, 3, 4, 'r'),
-(27, 'gt3', 'gttt', '2015-08-23 17:46:29', 1, 'f', NULL, 1, 0, 34, 3, 4, 'r'),
-(28, 'gt4', 'qe', '2015-08-23 17:47:39', 1, 'f', NULL, 3, 0, 1, 3, 4, 'qe'),
-(29, 'Maxi', 'pepe', '2015-08-23 17:49:27', 2, 'm', NULL, 3, 20, 100, 0, 1, 'maxi-cantarell@hotmail.com'),
-(30, 'fdfdafdafdfa', 'fadfad', '2015-08-23 17:56:42', 1, 'm', NULL, 2, 234, 5245, 3, 4, 'fadfadfa'),
-(31, '45245245', '45', '2015-08-23 18:05:48', 1, 'm', NULL, 2, 5454, 545, 3, 4, '454'),
-(32, '425245', '5245', '2015-08-23 18:11:13', 2, 'm', NULL, 2, 42524, 42524, 3, 4, '42524'),
-(33, 'LeanBiker', 'pepe', '2015-08-23 18:28:28', 1, 'm', NULL, 1, 21, 23, 3, 4, 'easa'),
-(34, '646', '6464', '2015-08-23 18:38:28', 1, 'm', NULL, 2, 446, 464, 3, 4, '4646'),
-(35, 'gdfa', 'fad', '2015-08-23 18:51:10', 1, 'm', NULL, 1, 234, 245, 3, 4, 'fad'),
-(36, 'lucas', '7410', '2015-08-24 22:20:29', 2, 'm', NULL, 2, 15, 174, 0, 3, 'luks_manga@hotmail.com');
+INSERT INTO `usuarios` (`IdUsuario`, `Usuario`, `Contrase`, `fechaCreacion`, `IdContextura`, `Sexo`, `Trabajo`, `IdRutina`, `Edad`, `Altura`, `IdPreexistente`, `IdDieta`, `Email`, `IdPesos-Ideales`) VALUES
+(1, 'Leandrin', 'pepe', '0000-00-00 00:00:00', 3, 'm', NULL, 3, 0, 123, 1, 2, 'pepin', 1),
+(3, 'd', 'pepe', '2015-08-23 16:42:59', 3, 'm', NULL, 3, 0, 123, 1, 3, 'pepin', 1),
+(4, '425', 'twr', '2015-08-23 17:02:37', 1, 'm', NULL, 3, 0, 0, 3, 4, '24524', 1),
+(5, 'fd', 'fda', '2015-08-23 17:05:43', 2, 'f', NULL, 3, 0, 452, 3, 4, 'fafd', 1),
+(6, 'fdafad', 'fdafa', '2015-08-23 17:10:53', 2, 'm', NULL, 1, 0, 134, 3, 4, 'adfad', 1),
+(8, 'fdafadd', 'fdafa', '2015-08-23 17:11:37', 2, 'm', NULL, 1, 0, 134, 3, 4, 'adfad', 1),
+(9, 'fdafaddd', 'fdafa', '2015-08-23 17:12:15', 2, 'm', NULL, 1, 0, 134, 3, 4, 'adfad', 1),
+(10, 'fdafadddg', 'fdafa', '2015-08-23 17:12:28', 2, 'm', NULL, 1, 0, 134, 3, 4, 'adfad', 1),
+(11, 'frgsfg', 'gsfgf', '2015-08-23 17:20:23', 1, 'f', NULL, 1, 0, 34, 3, 4, 'sfgsf', 1),
+(12, 'frgsfgh', 'gsfgf', '2015-08-23 17:20:45', 1, 'f', NULL, 1, 0, 34, 3, 4, 'sfgsf', 1),
+(13, 'the', 'hethe', '2015-08-23 17:21:54', 2, 'm', NULL, 2, 0, 2142, 3, 4, 'hethe', 1),
+(15, '4254', '24524', '2015-08-23 17:26:56', 1, 'm', NULL, 1, 0, 2452, 3, 4, '452452', 1),
+(16, '789', '789', '2015-08-23 17:28:16', 1, 'm', NULL, 3, 0, 789, 3, 4, '7789', 1),
+(17, '46', '4', '2015-08-23 17:30:14', 2, 'm', NULL, 2, 0, 34, 3, 4, '4', 1),
+(18, '423', '23', '2015-08-23 17:32:18', 1, 'm', NULL, 2, 0, 234, 3, 4, '234', 1),
+(19, 'er', 're', '2015-08-23 17:34:25', 2, 'm', NULL, 2, 0, 4, 3, 4, 're', 1),
+(20, 'feeqeq', 'fe', '2015-08-23 17:36:23', 1, 'm', NULL, 2, 0, 3, 3, 4, 'qeqeq', 1),
+(21, '2', '23', '2015-08-23 17:39:52', 1, 'm', NULL, 2, 0, 3, 3, 4, '234', 1),
+(22, '25', '23', '2015-08-23 17:40:47', 1, 'm', NULL, 2, 0, 3, 3, 4, '234', 1),
+(23, 'g', 'g', '2015-08-23 17:42:38', 1, 'f', NULL, 1, 0, 34, 3, 4, 'r', 1),
+(25, 'gt', 'gttt', '2015-08-23 17:45:01', 1, 'f', NULL, 1, 0, 34, 3, 4, 'r', 1),
+(26, 'gt2', 'gttt', '2015-08-23 17:46:01', 1, 'f', NULL, 1, 0, 34, 3, 4, 'r', 1),
+(27, 'gt3', 'gttt', '2015-08-23 17:46:29', 1, 'f', NULL, 1, 0, 34, 3, 4, 'r', 1),
+(28, 'gt4', 'qe', '2015-08-23 17:47:39', 1, 'f', NULL, 3, 0, 1, 3, 4, 'qe', 1),
+(29, 'Maxi', 'pepe', '2015-08-23 17:49:27', 2, 'm', NULL, 3, 20, 100, 2, 1, 'maxi-cantarell@hotmail.com', 1),
+(30, 'fdfdafdafdfa', 'fadfad', '2015-08-23 17:56:42', 1, 'm', NULL, 2, 234, 5245, 3, 4, 'fadfadfa', 1),
+(31, '45245245', '45', '2015-08-23 18:05:48', 1, 'm', NULL, 2, 5454, 545, 3, 4, '454', 1),
+(32, '425245', '5245', '2015-08-23 18:11:13', 2, 'm', NULL, 2, 42524, 42524, 3, 4, '42524', 1),
+(33, 'LeanBiker', 'pepe', '2015-08-23 18:28:28', 1, 'm', NULL, 1, 21, 23, 3, 4, 'easa', 1),
+(34, '646', '6464', '2015-08-23 18:38:28', 1, 'm', NULL, 2, 446, 464, 3, 4, '4646', 1),
+(35, 'gdfa', 'fad', '2015-08-23 18:51:10', 1, 'm', NULL, 1, 234, 245, 3, 4, 'fad', 1),
+(36, 'lucas', '7410', '2015-08-24 22:20:29', 2, 'm', NULL, 2, 15, 174, 1, 3, 'luks_manga@hotmail.com', 1);
 
 --
 -- Índices para tablas volcadas
@@ -632,13 +630,25 @@ ALTER TABLE `estaciones`
 --
 ALTER TABLE `grupos`
   ADD PRIMARY KEY (`IdGrupo`),
-  ADD UNIQUE KEY `Grupo` (`Nombre`);
+  ADD UNIQUE KEY `Grupo` (`Nombre`),
+  ADD KEY `IdUsuarioCreador` (`IdUsuarioCreador`);
+
+--
+-- Indices de la tabla `historial-horarios`
+--
+ALTER TABLE `historial-horarios`
+  ADD PRIMARY KEY (`IdRecetaHorario`),
+  ADD KEY `IdHistorial` (`IdHistorial`),
+  ADD KEY `IdHorario` (`IdHorario`);
 
 --
 -- Indices de la tabla `historiales`
 --
 ALTER TABLE `historiales`
-  ADD PRIMARY KEY (`IdHistoria`);
+  ADD PRIMARY KEY (`IdHistoria`),
+  ADD KEY `IdAccion` (`IdAccion`),
+  ADD KEY `IdUsuario` (`IdUsuario`),
+  ADD KEY `IdReceta` (`IdReceta`);
 
 --
 -- Indices de la tabla `horarios`
@@ -680,43 +690,50 @@ ALTER TABLE `preexistentes`
 -- Indices de la tabla `preferencias`
 --
 ALTER TABLE `preferencias`
-  ADD PRIMARY KEY (`IdPreferencia`);
+  ADD PRIMARY KEY (`IdPreferencia`),
+  ADD KEY `IdUsuario` (`IdUsuario`),
+  ADD KEY `IdIngrediente` (`IdIngrediente`);
 
 --
 -- Indices de la tabla `procedimiento`
 --
 ALTER TABLE `procedimiento`
-  ADD PRIMARY KEY (`IdProcedimiento`);
+  ADD PRIMARY KEY (`IdProcedimiento`),
+  ADD KEY `IdReceta` (`IdReceta`),
+  ADD KEY `IdPaso` (`IdPaso`);
 
 --
 -- Indices de la tabla `puntuaciones`
 --
 ALTER TABLE `puntuaciones`
-  ADD PRIMARY KEY (`IdPuntuacion`);
+  ADD PRIMARY KEY (`IdPuntuacion`),
+  ADD KEY `IdReceta` (`IdReceta`),
+  ADD KEY `IdUsuario` (`IdUsuario`);
 
 --
 -- Indices de la tabla `receta-estaciones`
 --
 ALTER TABLE `receta-estaciones`
-  ADD PRIMARY KEY (`IdRecetaEstacion`);
-
---
--- Indices de la tabla `receta-horarios`
---
-ALTER TABLE `receta-horarios`
-  ADD PRIMARY KEY (`IdRecetaHorario`);
+  ADD PRIMARY KEY (`IdRecetaEstacion`),
+  ADD KEY `IdEstacion` (`IdEstacion`),
+  ADD KEY `IdReceta` (`IdReceta`);
 
 --
 -- Indices de la tabla `receta-ingredientes`
 --
 ALTER TABLE `receta-ingredientes`
-  ADD PRIMARY KEY (`IdRecetaIngrediente`);
+  ADD PRIMARY KEY (`IdRecetaIngrediente`),
+  ADD KEY `IdReceta` (`IdReceta`);
 
 --
 -- Indices de la tabla `recetas`
 --
 ALTER TABLE `recetas`
-  ADD PRIMARY KEY (`IdReceta`);
+  ADD PRIMARY KEY (`IdReceta`),
+  ADD KEY `IdDificultad` (`IdDificultad`),
+  ADD KEY `IdUsuario` (`IdUsuario`),
+  ADD KEY `IdPiramide` (`IdPiramide`),
+  ADD KEY `IdDieta` (`IdDieta`);
 
 --
 -- Indices de la tabla `rutinas`
@@ -728,14 +745,21 @@ ALTER TABLE `rutinas`
 -- Indices de la tabla `usuario-grupos`
 --
 ALTER TABLE `usuario-grupos`
-  ADD PRIMARY KEY (`IdUsuarioGrupo`);
+  ADD PRIMARY KEY (`IdUsuarioGrupo`),
+  ADD KEY `IdUsuario` (`IdUsuario`),
+  ADD KEY `IdGrupo` (`IdGrupo`);
 
 --
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`IdUsuario`),
-  ADD UNIQUE KEY `Usuario` (`Usuario`);
+  ADD UNIQUE KEY `Usuario` (`Usuario`),
+  ADD KEY `IdContextura` (`IdContextura`),
+  ADD KEY `IdRutina` (`IdRutina`),
+  ADD KEY `IdDieta` (`IdDieta`),
+  ADD KEY `IdPreexistente` (`IdPreexistente`),
+  ADD KEY `IdPesos-Ideales` (`IdPesos-Ideales`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -766,6 +790,101 @@ ALTER TABLE `dietas`
 --
 ALTER TABLE `dificultades`
   MODIFY `IdDificultad` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT de la tabla `pesos-ideales`
+--
+ALTER TABLE `pesos-ideales`
+  MODIFY `IdPesos-Ideales` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT de la tabla `preferencias`
+--
+ALTER TABLE `preferencias`
+  MODIFY `IdPreferencia` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `grupos`
+--
+ALTER TABLE `grupos`
+  ADD CONSTRAINT `UsuarioCreador` FOREIGN KEY (`IdUsuarioCreador`) REFERENCES `usuarios` (`IdUsuario`);
+
+--
+-- Filtros para la tabla `historial-horarios`
+--
+ALTER TABLE `historial-horarios`
+  ADD CONSTRAINT `Historial` FOREIGN KEY (`IdHistorial`) REFERENCES `historiales` (`IdHistoria`),
+  ADD CONSTRAINT `Horarios` FOREIGN KEY (`IdHorario`) REFERENCES `horarios` (`IdHorario`);
+
+--
+-- Filtros para la tabla `historiales`
+--
+ALTER TABLE `historiales`
+  ADD CONSTRAINT `Historial-Accion` FOREIGN KEY (`IdAccion`) REFERENCES `acciones` (`IdAccion`),
+  ADD CONSTRAINT `Historial-Receta` FOREIGN KEY (`IdReceta`) REFERENCES `recetas` (`IdReceta`),
+  ADD CONSTRAINT `Historial-Usuario` FOREIGN KEY (`IdUsuario`) REFERENCES `usuarios` (`IdUsuario`);
+
+--
+-- Filtros para la tabla `preferencias`
+--
+ALTER TABLE `preferencias`
+  ADD CONSTRAINT `Ingrediente` FOREIGN KEY (`IdIngrediente`) REFERENCES `ingredientes` (`IdIngrediente`),
+  ADD CONSTRAINT `Preferencia-Usuario` FOREIGN KEY (`IdUsuario`) REFERENCES `usuarios` (`IdUsuario`);
+
+--
+-- Filtros para la tabla `procedimiento`
+--
+ALTER TABLE `procedimiento`
+  ADD CONSTRAINT `Pasos` FOREIGN KEY (`IdPaso`) REFERENCES `pasos` (`IdPasos`),
+  ADD CONSTRAINT `Recetas` FOREIGN KEY (`IdReceta`) REFERENCES `recetas` (`IdReceta`);
+
+--
+-- Filtros para la tabla `puntuaciones`
+--
+ALTER TABLE `puntuaciones`
+  ADD CONSTRAINT `Puntuacion-Receta` FOREIGN KEY (`IdReceta`) REFERENCES `recetas` (`IdReceta`),
+  ADD CONSTRAINT `Puntuacion-Usuario` FOREIGN KEY (`IdUsuario`) REFERENCES `usuarios` (`IdUsuario`);
+
+--
+-- Filtros para la tabla `receta-estaciones`
+--
+ALTER TABLE `receta-estaciones`
+  ADD CONSTRAINT `Estaciones` FOREIGN KEY (`IdEstacion`) REFERENCES `estaciones` (`IdEstacion`),
+  ADD CONSTRAINT `Receta-Estaciones2` FOREIGN KEY (`IdReceta`) REFERENCES `recetas` (`IdReceta`);
+
+--
+-- Filtros para la tabla `receta-ingredientes`
+--
+ALTER TABLE `receta-ingredientes`
+  ADD CONSTRAINT `Receta-Ingredientes` FOREIGN KEY (`IdReceta`) REFERENCES `recetas` (`IdReceta`);
+
+--
+-- Filtros para la tabla `recetas`
+--
+ALTER TABLE `recetas`
+  ADD CONSTRAINT `Receta-Dieta` FOREIGN KEY (`IdDieta`) REFERENCES `dietas` (`IdDieta`),
+  ADD CONSTRAINT `Receta-Dificultad` FOREIGN KEY (`IdDificultad`) REFERENCES `dificultades` (`IdDificultad`),
+  ADD CONSTRAINT `Receta-Piramide` FOREIGN KEY (`IdPiramide`) REFERENCES `piramides` (`IdPiramide`),
+  ADD CONSTRAINT `Receta-Usuario` FOREIGN KEY (`IdUsuario`) REFERENCES `usuarios` (`IdUsuario`);
+
+--
+-- Filtros para la tabla `usuario-grupos`
+--
+ALTER TABLE `usuario-grupos`
+  ADD CONSTRAINT `Grupo` FOREIGN KEY (`IdGrupo`) REFERENCES `grupos` (`IdGrupo`),
+  ADD CONSTRAINT `Grupo-Usuario` FOREIGN KEY (`IdUsuario`) REFERENCES `usuarios` (`IdUsuario`);
+
+--
+-- Filtros para la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `Usuario-Contextura` FOREIGN KEY (`IdContextura`) REFERENCES `contexturas` (`IdContextura`),
+  ADD CONSTRAINT `Usuario-Dieta` FOREIGN KEY (`IdDieta`) REFERENCES `dietas` (`IdDieta`),
+  ADD CONSTRAINT `Usuario-Peso` FOREIGN KEY (`IdPesos-Ideales`) REFERENCES `pesos-ideales` (`IdPesos-Ideales`),
+  ADD CONSTRAINT `Usuario-Preexistentes` FOREIGN KEY (`IdPreexistente`) REFERENCES `preexistentes` (`IdPreexistente`),
+  ADD CONSTRAINT `Usuario-Rutina` FOREIGN KEY (`IdRutina`) REFERENCES `rutinas` (`IdRutina`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
